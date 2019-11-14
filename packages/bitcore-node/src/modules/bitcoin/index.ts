a20927b13519eed665d6ea6fb6b048f4ccb42a2d
Rowan de Haas <rowandehaas@outlook.com>
2019-11-14 21:32:30 +1100

    Uppercase

--- packages/bitcore-node/src/modules/bitcoin/index.ts
+++ packages/bitcore-node/src/modules/bitcoin/index.ts
@@ -5,8 +5,8 @@ import { BTCStateProvider } from "../../providers/chain-state/btc/btc";
 export default class BitcoinModule extends BaseModule {
   constructor(services: BaseModule["bitcoreServices"]) {
     super(services);
-    services.Libs.register('stratis', 'bitcore-lib', 'bitcore-p2p');
-    services.P2P.register('stratis', BitcoinP2PWorker);
-    services.CSP.registerService('stratis', new BTCStateProvider());
+    services.Libs.register('STRATIS', 'bitcore-lib', 'bitcore-p2p');
+    services.P2P.register('STRATIS', BitcoinP2PWorker);
+    services.CSP.registerService('STRATIS', new BTCStateProvider());
   }
 }
