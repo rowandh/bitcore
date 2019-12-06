6dfc70d63eb69259d4e2cead36be0e6431a08eb9
Rowan de Haas <rowandehaas@outlook.com>
2019-12-06 17:03:20 +1100

    Constants

--- packages/bitcore-wallet-client/src/lib/common/constants.ts
+++ packages/bitcore-wallet-client/src/lib/common/constants.ts
@@ -96,8 +96,39 @@ export const Constants = {
         minDecimals: 0,
       }
     },
+    strat: {
+      toSatoshis: 100000000,
+      full: {
+        maxDecimals: 8,
+        minDecimals: 8,
+      },
+      short: {
+        maxDecimals: 6,
+        minDecimals: 2,
+      }
+    },
   },
-  COINS: ['btc', 'bch', 'eth', 'usdc', 'pax', 'gusd'],
+  COINS: ['btc', 'bch', 'eth', 'usdc', 'pax', 'gusd', 'strat'],
   ERC20: ['usdc', 'pax', 'gusd'],
-  UTXO_COINS: ['btc', 'bch']
+  UTXO_COINS: ['btc', 'bch', 'strat'],
+  TOKEN_OPTS: {
+    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      name: 'USD Coin',
+      symbol: 'USDC',
+      decimal: 6,
+      address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+    },
+    '0x8e870d67f660d95d5be530380d0ec0bd388289e1': {
+      name: 'Paxos Standard',
+      symbol: 'PAX',
+      decimal: 18,
+      address: '0x8e870d67f660d95d5be530380d0ec0bd388289e1'
+    },
+    '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd': {
+      name: 'Gemini Dollar',
+      symbol: 'GUSD',
+      decimal: 2,
+      address: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd'
+    },
+  },
 };
