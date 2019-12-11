89f735baf7af3ec7b5d0c969ef5309d3991428cc
Rowan de Haas <rowandehaas@outlook.com>
2019-12-10 20:57:00 -0800

    Update copay reference to bitcore-wallet-client-stratis

--- packages/copay/src/providers/bwc/bwc.ts
+++ packages/copay/src/providers/bwc/bwc.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Logger } from '../../providers/logger/logger';
 
-import BWC from 'bitcore-wallet-client';
+import BWC from 'bitcore-wallet-client-stratis';
 
 @Injectable()
 export class BwcProvider {
