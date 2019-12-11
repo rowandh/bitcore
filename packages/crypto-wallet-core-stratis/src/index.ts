508dbd94935516dc9b2e15250008e890591f71da
Rowan de Haas <rowandehaas@outlook.com>
2019-12-10 20:49:19 -0800

    Add crypto-wallet-core-stratis

---
+++ packages/crypto-wallet-core-stratis/src/index.ts
@@ -0,0 +1,7 @@
+import * as BitcoreLibCash from 'bitcore-lib-cash';
+import * as BitcoreLib from 'bitcore-lib-stratis';
+import Web3 from 'web3';
+import Deriver from './derivation';
+import Transactions from './transactions';
+import Validation from './validation';
+export { BitcoreLib, BitcoreLibCash, Deriver, Transactions, Validation, Web3 };
